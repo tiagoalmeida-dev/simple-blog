32fdcb54fd2c8590f029ed15063e240f1c964958
web-dev-fan <pro.dev.work@hotmail.com>
2020-03-21 23:23:58 -0300

    Add gravatar profile image

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,2 +1,8 @@
+require 'digest'
+
 module AuthorsHelper
+  def load_author_avatar(email)
+    hash = Digest::MD5.hexdigest(email.downcase)
+    image_src = "https://www.gravatar.com/avatar/#{hash}?d=#{request.base_url}/avatar.png&s=200"
+  end
 end
