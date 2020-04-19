203e1aa82efd90db3406b0323e098d3a9a42a59c
web-dev-fan <pro.dev.work@hotmail.com>
2020-04-19 17:18:23 -0300

    Show active page

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
 module ApplicationHelper
+  def nav_link_to(text, path, opts)
+    options = current_page?(path) ? { class: "active" } : {}
+    content_tag(:li, options) do
+      link_to text, path, opts
+    end
+  end
 end
