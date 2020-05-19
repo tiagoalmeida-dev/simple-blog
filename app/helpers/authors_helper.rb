e05c9658a1a268b8ac8f372c15e9b78884c4089b
web-dev-fan <pro.dev.work@hotmail.com>
2020-05-18 21:22:05 -0300

    Update profile image size

--- app/helpers/authors_helper.rb
+++ app/helpers/authors_helper.rb
@@ -1,7 +1,7 @@
 require 'digest'
 
 module AuthorsHelper
-  def load_author_avatar(email, size=400)
+  def load_author_avatar(email, size=200)
     hash = Digest::MD5.hexdigest(email.downcase)
     gravatar_url = "https://www.gravatar.com/avatar/"
     image_src = "#{gravatar_url + hash}?s=#{size}"
